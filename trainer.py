eca1cd8a399afc8d86f733fc9dcbea6f50134572
Cho Hye Min <pedalioaspare@gmail.com>
2018-05-10 14:12:29 -0700

    Uncomment add img

--- trainer.py
+++ trainer.py
@@ -236,8 +236,8 @@ class Trainer(object):
             tf.summary.image("AE_x", self.AE_x),
 
             # MEE Visualize Kernels
-            tf.summary.image(self.ConvWeights.name, put_kernels_on_grid(self.ConvWeights), max_outputs=1)
-            tf.summary.image(self.Conv1Weights.name, put_kernels_on_grid(self.Conv1Weights), max_outputs=1)
+            # tf.summary.image(self.ConvWeights.name, put_kernels_on_grid(self.ConvWeights), max_outputs=1)
+            # tf.summary.image(self.Conv1Weights.name, put_kernels_on_grid(self.Conv1Weights), max_outputs=1)
 
             tf.summary.scalar("loss/d_loss", self.d_loss),
             tf.summary.scalar("loss/d_loss_real", self.d_loss_real),
