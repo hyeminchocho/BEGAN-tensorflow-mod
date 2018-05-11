3186a366e2a4e386940396d001a761178287392b
Cho Hye Min <pedalioaspare@gmail.com>
2018-05-11 14:41:01 -0700

    Fix r1 to z1, not run trainer test - fix

--- trainer.py
+++ trainer.py
@@ -350,7 +350,7 @@ class Trainer(object):
         z2 = np.random.uniform(-1, 1, size=(1, self.z_num))
         for idx, ratio in enumerate(np.linspace(0, 1, 10)):
             z = np.stack([slerp(ratio, r1, r2) for r1, r2 in zip(z1, z2)])
-            x = self.sess.run(self.G, {self.z: inputs})
+            x = self.sess.run(self.G, {self.z: z})
             save_one_image(x[0,:, :,:], "./interp_{}_G.jpg".format(idx))
             # z_decode = self.generate(z, save=False)
             # generated.append(z_decode)
